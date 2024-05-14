1a7ed3b29b45bd57555b0fb8bf967455f40a21fe
jumokeoladimeji <jumoke5ng@yahoo.com>
2024-05-14 12:12:58 +0100

    display list of artwork

--- pages/api/artwork-details.js
+++ pages/api/artwork-details.js
@@ -1,3 +1,4 @@
+import { NextResponse } from "next/server";
 import connectToDatabase from '../../lib/mongodb';
 
 export default async function handler(req, res) {
@@ -12,7 +13,21 @@ export default async function handler(req, res) {
       console.error('Error saving data:', error);
       res.status(500).json({ message: 'Error saving data' });
     }
-  } else {
+  }
+  
+  if (req.method === 'GET') {
+    const collection = await connectToDatabase();
+    try {
+      const imageList = await collection.find({}).toArray();
+      res.status(200).json({ message: 'Data retrieved successfully', data: imageList });
+      // return NextResponse.json({ imageList }, { status: 200 });
+    } catch (err) {
+      res.status(500).json({ message: 'Error retrieving data' });
+      // return NextResponse.json({ message: "Error", err }, { status: 500 });
+    }
+  }
+  else {
     res.status(405).json({ message: 'Method Not Allowed' });
   }
+
 }
---
+++ pages/artworks.js
@@ -0,0 +1,56 @@
+import { useState} from "react";
+import { saveAs } from 'file-saver';
+
+export default function Artworks({ artworks }) {
+//   const [artworkData, setArtworks] = useState([]);
+//   const [isLoading, setIsLoading] = useState(false)
+
+
+    const downloadQRCode = (artwork) => {
+        // saveAs(artwork.qrImageUrl, `${artwork.artworkName}-by-${artwork.artistName}-qrcode.png`);
+        // setIsLoading(true)
+    };
+
+  return (
+    <div className="overflow-y-auto mx-auto mx-6 my-6">
+        {artworks && artworks.map((artwork, i) => (
+            <div className="flex flex-col mt-2" key={artwork._id}>
+                <article className="flex m-t-6 items-center bg-gray-100">
+                {/* <a href="#" className="flex flex-col items-center bg-white border border-gray-200 rounded-lg shadow md:flex-row md:max-w-xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"> */}
+                    <img className="object-cover w-full rounded-t-lg h-96 md:h-auto md:w-48 md:rounded-none md:rounded-s-lg" 
+                        src={artwork?.cloudinaryUrl && artwork?.cloudinaryUrl} alt="" 
+                    />
+                    <div className="flex flex-col justify-between p-4 leading-normal">
+                        <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white"> {artwork.artworkName}</h5>
+                        by <span>{artwork.artistName}</span>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">{artwork.description}</p>
+                        <p>View on the website: {artwork.artWorkURL}</p>
+                    </div>
+                    {/* <button
+                        className="flex flex-col mt-10 bg-yellow-800 hover:bg-yellow-600 text-white font-bold py-2 px-4 w-full rounded"
+                        onClick={downloadQRCode(artwork)}
+                        disabled={!artwork?.cloudinaryUrl}
+                    >
+                    Download PNG
+                        
+                    </button> */}
+                {/* </a> */}
+                </article>
+            </div>
+        ))}
+    </div>
+  );
+}
+
+export async function getServerSideProps(ctx) {
+	// try {
+        const response = await fetch(`http://localhost:3000/api/artwork-details`);
+        if (response.ok) {
+            const artworks = await response.json();
+            console.log('artworks::', artworks.data)
+            // setArtworks(artworks.data)
+            // setIsLoading(true)
+            return { props: { artworks: artworks.data }}
+        }
+    
+}
