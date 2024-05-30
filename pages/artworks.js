984ca9d8130330a11c04a750049ea6bada282997
jumokeoladimeji <jumoke5ng@yahoo.com>
2024-05-30 11:16:53 +0100

    update artworks

--- pages/artworks.js
+++ pages/artworks.js
@@ -1,14 +1,6 @@
 import React, { useState } from 'react';
 import { usePDF } from 'react-to-pdf';
-import Table from '../components/Table';
-import { exportSelectedRowsToPDF } from '../utils/exportToPdf';
-
-const data = [
-    { name: 'John Doe', age: 28, address: '123 Main St' },
-    { name: 'Jane Smith', age: 34, address: '456 Maple Ave' },
-    { name: 'Sam Green', age: 45, address: '789 Oak Dr' },
-];
-  
+import Card from '../components/Card';
 
 export default function ArtworkList({ artworks }) {
     // const [selectedArtworks, setSelectedArtworks] = useState([]);
@@ -30,53 +22,45 @@ export default function ArtworkList({ artworks }) {
     	filename: 'muliple-qrcodes.pdf',
     });
 
-    return (
-        <div className="mx-6 my-6">
-            <div className=" border-slate-200 rounded-lg p-6 border-2">
-                <div className="justify-between items-center text-center mb-3">
-                    <h1
-                        style={{ fontFamily: 'Lobster Two' }}
-                        className="text-2xl"
-                    >
-                        QRCODES
-                    </h1>
-                </div>
-                <button
-                    // onClick={() => exportSelectedRowsToPDF('selected-rows-to-pdf')}
-                    onClick={() => toPDF()}
-                    className="mt-4 px-4 py-2 bg-[#000] hover:bg-[#7b7c7c] text-white rounded"
-                >
-                    Export Selected Rows to PDF
-                </button>
-                {selectedRows.length > 0 && (
-                <div className='text-center'>
-                    <h1
-                        style={{ fontFamily: 'Lobster Two' }}
-                        className="text-xl"
-                    >
-                        SELECTED ROWS
-                    </h1>
-                    <div id="selected-rows-to-pdf" ref={targetRef}>
-                        <Table data={selectedData} selectedRows={selectedRows} onSelectRow={() => {}} />
+    if (artworks) {
+        return (
+            <div className="mx-6 my-6">
+                <div className="border-slate-200 rounded-lg p-6 border-2">
+                    <div className="justify-between items-center text-center mb-3">
+                        <h1
+                            style={{ fontFamily: 'Lobster Two' }}
+                            className="text-2xl"
+                        >
+                            TAGS
+                        </h1>
                     </div>
-                </div>
-                )}
-                <div>
-                <div id="table-to-pdf">
-                    <Table data={artworks} selectedRows={selectedRows} onSelectRow={handleSelectRow} />
-                </div></div>
+                    <button
+                        // onClick={() => exportSelectedRowsToPDF('selected-rows-to-pdf')}
+                        onClick={() => toPDF()}
+                        className="mt-4 mb-4 px-4 py-2 bg-[#000] hover:bg-[#7b7c7c] text-white rounded"
+                    >
+                        Export To PDF
+                    </button>
+                    <div>
+                        <div id="table-to-pdf" ref={targetRef}>
+                            <Card data={artworks} />
+                        </div>
+                    </div>
 
+                </div>
             </div>
-        </div>
-  );
+    );
+    } else {
+        return <div>loading...</div>
+    }
 }
 
 export async function getServerSideProps(ctx) {
 	// try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/artwork-details`);
-        if (response.ok) {
-            const artworks = await response.json();
-            return { props: { artworks: artworks.data }}
-        }
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/artwork-details`);
+    if (response.ok) {
+        const artworks = await response.json();
+        return { props: { artworks: artworks.data }}
+    }
     
 }
